33f84cf3da414c644aa95e9251abe2bb4ed6d449
CIMSOLUTIONS\r.visser <rensv1991@gmail.com>
2017-12-06 16:13:23 +0100

    Added a vieuw features.

--- js/addOrder.js
+++ js/addOrder.js
@@ -1,34 +1,91 @@
-$(document).ready(
-		function() {
-			$("#addOrderSubmitButton").click(
-					function(event) {
-						event.preventDefault();
-						var dateTime = new Date($("#dateOrder").val());
-						var jsonData = {
-							customer_id : $("#customerId").val(),
-							order_type : $("#orderType").val(),
+function generatesSelectOption(value, textToDislpay) {
+	return "<option value=\"" + value + "\">" + textToDislpay + "</option>"
+}
 
-							dateAndTimeOrderMade : [ dateTime.getFullYear(),
-									dateTime.getMonth(), dateTime.getDate(),
-									dateTime.getHours(), dateTime.getMinutes(),
-									dateTime.getSeconds(),
-									dateTime.getMilliseconds() ],
-							orderReceived : false,
-							orderDelivered : false
-						};
-						jsonData = JSON.stringify(jsonData);
-						console.log(jsonData);
-						$.ajax({
-							type : "POST",
+$(document)
+		.ready(
+				 $( '#someid' ).on( 'click', function (event) { console.log('hello') }),
+				$("#addOrderSubmitButton").click(
+						function(event) {
+							event.preventDefault();
+							console.log("Hello World");
+							var dateTime = new Date($("#dateOrder").val());
+							var jsonData = {
+								customer_id : $("#customerId").val(),
+								order_type : $("#orderType").val(),
+
+								dateAndTimeOrderMade : [
+										dateTime.getFullYear(),
+										dateTime.getMonth(),
+										dateTime.getDate(),
+										dateTime.getHours(),
+										dateTime.getMinutes(),
+										dateTime.getSeconds(),
+										dateTime.getMilliseconds() ],
+								orderReceived : false,
+								orderDelivered : false
+							};
+							jsonData = JSON.stringify(jsonData);
+							console.log(jsonData);
+							$.ajax({
+								type : "POST",
+								dataType : "json",
+								contentType : "application/json",
+								url : "http://localhost:1234/orders/order/add",
+								data : jsonData
+
+							}).then(function(data) {
+								console.log(jsonData);
+							});
+
+						}),
+
+				$
+						.ajax({
+							type : "GET",
 							dataType : "json",
 							contentType : "application/json",
-							url : "http://localhost:1234/orders/order/add",
-							data : jsonData
+							url : "http://localhost:1234/articles/articles",
+						})
+						.then(
+								function(data) {
+									var htmlToPlace = "<label for=\"article_id\">Artikel:</label>";
+									htmlToPlace += "<select name=\"article_id\" form=\"addOrder\" id=\"article_id\">";
 
-						}).then(function(data) {
-							console.log(jsonData);
-						});
+									Object
+											.keys(data)
+											.forEach(
+													function(key) {
+														htmlToPlace += generatesSelectOption(
+																data[key].article_id,
+																data[key].article_name);
+													});
 
-					});
+									htmlToPlace += "</select>";
+									$("#articlePlaceHolder").html(htmlToPlace);
+								}),
 
-		});
+				$
+						.ajax({
+							type : "GET",
+							dataType : "json",
+							contentType : "application/json",
+							url : "http://localhost:1234/customers/customers",
+						})
+						.then(
+								function(data) {
+									var htmlToPlace = "<label for=\"customerId\">Customer:</label>";
+									htmlToPlace += "<select name=\"customerId\" form=\"addOrder\" id=\"customerId\">";
+
+									Object
+											.keys(data)
+											.forEach(
+													function(key) {
+														htmlToPlace += generatesSelectOption(
+																data[key].customer_id,
+																data[key].customer_name);
+													});
+
+									htmlToPlace += "</select>";
+									$("#customerPlaceHolder").html(htmlToPlace);
+								}));
