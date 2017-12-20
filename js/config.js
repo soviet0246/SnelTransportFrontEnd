ed9942ba71770c8bbed6b46ff269db299c9c6152
CIMSOLUTIONS\r.visser <rensv1991@gmail.com>
2017-12-20 11:45:48 +0100

    Added config file.

---
+++ js/config.js
@@ -0,0 +1,8 @@
+var URL_JAVA = "http://172.16.1.186/sneltransport/";
+var URL_CSHARP = "http://172.16.1.186/Service_Database_Connection/Service.svc/rest/" ;
+var URL_UNDELIVERED_ORDERS = "orders/orders";
+var URL_SPECIFIC_ORDER = "orders/order/";
+var URL_ALL_CUSTOMERS = "custoemrs/customers";
+var URL_SPECIFIC_CUSTOMER = "customers/customer/";
+var URL_OPTIMAL_ROUTE_GET = "GetOptimal";
+
